56a644e3f4cbe899d1d4faca449594c9fe9d2a81
courage <akpancourage52@gmail.com>
2026-04-21 11:53:44 +0100

    feat: define Profile model with UUID v7 and demographic fields

---
+++ stage1_stage2/models.py
@@ -0,0 +1,21 @@
+from extensions import db
+from datetime import datetime
+
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+    gender = db.Column(db.String)
+    gender_probability = db.Column(db.Float)
+    sample_size = db.Column(db.Integer)
+
+    age = db.Column(db.Integer)
+    age_group = db.Column(db.String)
+
+    country_id = db.Column(db.String(2))
+    country_name = db.Column(db.String)
+    country_probability = db.Column(db.Float)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
